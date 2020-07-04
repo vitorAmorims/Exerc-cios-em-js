4bde33b7178542e661c14c5b27766e40a1a603a5
Vitor Amorims <vtamorims@gmail.com>
2020-07-03 21:34:24 -0300

    Exercício palindromo
    Objetivo: identificar palavras palindromos no array.

---
+++ ex06Palindromo/script.js
@@ -0,0 +1,27 @@
+var fpalavra = window.document.getElementById('textPalindromo')
+var tab = window.document.getElementById('flista') // chamando select do formulário
+var res = window.document.getElementById('res') // retornar resposta
+var vet = [];
+var palindromo = [];
+
+function analisar () {
+    var palavra = String(fpalavra.value)
+        vet.push(palavra);
+        palindromo.push(palavra.split('').reverse().join(''));
+        let item = document.createElement('option')
+        item.text = `${palavra}`
+        tab.appendChild(item)
+}   
+
+
+function finalizar (){
+    window.alert("Obrigado senhor JESUS, eu agradeço por me capacitar!")
+    for (let i = 0; i < vet.length; i++) {
+        if (vet[i] == palindromo[i]){
+            res.innerHTML = `A palavra ${vet[i]} é palindromo.`
+        } else {
+            res.innerHTML = `Não existem palindromo(s).`
+        }
+    }
+}
+
