37c4391f07a045ba30d0e2d009de5c909ed2fbb1
Vitor Amorims <vtamorims@gmail.com>
2020-07-08 17:15:00 -0300

    Desafio 01 js
    Aqui estou expondo primeiro desafio em javascript do professor Gustavo Guanabara.

---
+++ ex01MensagemAlerta/script.js
@@ -0,0 +1,17 @@
+var res = window.document.getElementById('res')
+
+function botao1 () {
+    window.alert(`você clicou no primeiro botão`)
+    res.innerHTML = (`usuário clicou no primeiro botão`)
+}
+
+function botao2 () {
+    window.alert(`você clicou no segundo botão`)
+    res.innerHTML = (`usuário clicou no segundo botão`)
+}
+
+function botao3 () {
+    window.alert(`você clicou no terceiro botão`)
+    res.innerHTML = (`usuário clicou no terceiro botão`)
+}
+
