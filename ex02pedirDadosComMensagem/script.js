9a948d7419fee8002797b3734e837ca8dc43ccdb
Vitor Amorims <vtamorims@gmail.com>
2020-07-08 17:47:56 -0300

    Ex2 desafio 2 prof Gustavo Guanabara
    Neste exercíćio, atribui uma variável para cada linha de comando window.prompt.
    Utilizei place rouder ${} para retornar mensagem ao usuário.

---
+++ ex02pedirDadosComMensagem/script.js
@@ -0,0 +1,6 @@
+function Idesafio () {
+    var nome = String(window.prompt(`Qual seu nome?`))
+    var idade = Number(window.prompt(`Olá ${nome}, qual sua idade?`))
+    window.alert(`Acabei de conhecer ${nome}, você possui ${idade} anos.`)
+    window.alert(`DESEJO MUITO SUCESSO EM SUA VIDA!`)
+}
