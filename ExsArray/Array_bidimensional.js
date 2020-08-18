e512c619c23b5214f4bf962f0b8a3e5a0548bbc0
Vitor Amorims <vtamorims@gmail.com>
2020-08-18 12:01:42 -0300

    Array Bidimensional com estruturas de repetição

--- ExsArray/Array_bidimensional.js
+++ ExsArray/Array_bidimensional.js
@@ -52,12 +52,12 @@ for (var i = 0; i < agenda.length -1; i++){
 console.table(agenda); //ascendente
 
 //rotina de ordenação dos descendente a partir do campo nome.
-//aqui farei loop for na rotina de incremento do indice
+//aqui farei loop for na rotina de incremento do indice.
 for (var i =0; i < agenda.length-1; i++){
     for (var j = i +1; j < agenda.length; j++){
         var x;
         if (agenda[i][0] < agenda[j][0]){
-            for(var k=0; k<agenda.length; k++){ //mais estiloso, gostei!!
+            for(var k=0; k< agenda.length; k++){ //mais estiloso, gostei!!
                 x=agenda[i][k];
                 agenda[i][k] = agenda[j][k];
                 agenda[j][k] = x;
@@ -71,4 +71,45 @@ console.table(agenda);
 Desenvolver um programa que leia os nomes de oito alunos e também suas quatro notas bimestrais. Ao final, deve apresentar o nome de cada aluno classificado em ordem alfabética, bem como suas médias e a média geral dos oito alunos.
 */
 var nomes = new Array;
-//pendente fazer o exercício.
+var nome1 = ['Vitor',10,5,7,9];
+var nome2 = ['Lucas',3,7,10,10];
+var nome3 = ['Silvia', 7, 9, 10,5];
+var nome4 = ['Neusa', 5,7,9,10];
+var nome5 = ['Rubens', 8,10,7,9];
+var nome6 = ['Ayrton', 9,8,10,7];
+var nome7 = ['Senna', 9,10,8,7];
+var nome8 = ['Tesla', 10,9,8,5];
+nomes.push(nome1,nome2,nome3,nome4,nome5,nome6,nome7,nome8);
+console.table(nomes);
+
+
+//rotina de ordenação dos ascendente a partir do campo nome.
+//aqui farei loop for na rotina de incremento do indice.
+for (var i =0; i < nomes.length-1; i++){
+    for (var j = i +1; j < nomes.length; j++){
+        var x;
+        if (nomes[i][0] > nomes[j][0]){
+            for(var k=0; k < 5; k++){ // obs: o número 5 controle o número de colunas.
+                x=nomes[i][k];
+                nomes[i][k] = nomes[j][k];
+                nomes[j][k] = x;
+            }
+        }
+    }
+}
+console.table(nomes); //aqui está ordenado ascendente pela coluna nome.
+
+//pendente media de cada aluno
+console.log((nomes[0][1]+nomes[0][2]+nomes[0][3]+nomes[0][4])/4);//manual apenas para entendimento
+/*fazer estrutura de repetição travando a linha 0, coluna 1 ate 4
+exibir o nome do aluno e sua respectiva média
+*/
+
+
+//pendente media geral por nota bimestral
+
+
+
+//pendente media geral dos alunos
+
+
