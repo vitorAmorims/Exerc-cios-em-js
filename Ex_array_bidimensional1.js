605bf6eba9e56638b5d54d84ee25af513ccc08d2
Vitor Amorims <vtamorims@gmail.com>
2020-08-22 12:36:51 -0300

    Criaçao da matriz B

--- Ex_array_bidimensional1.js
+++ Ex_array_bidimensional1.js
@@ -7,6 +7,13 @@ var matriz2 = new Array;
 var matriz3 = new Array;
 var matriz4 = new Array;
 
+var matriz5 = new Array;
+var matriz6 = new Array;
+var matriz7 = new Array;
+var matriz8 = new Array;
+var matriz9 = new Array;
+
+
 var matriz1A = new Array;
 var matriz1B = new Array;
 var matriz1C = new Array;
@@ -23,4 +30,19 @@ matriz1A[2]=matriz2;
 matriz1A[3]=matriz3;
 matriz1A[4]=matriz4;
 
+for (let i = 0; i < 3; i++){
+    matriz5.push(Math.floor(Math.random() * 25 +1));
+    matriz6.push(Math.floor(Math.random() * 25 +1));
+    matriz7.push(Math.floor(Math.random() * 25 +1));
+    matriz8.push(Math.floor(Math.random() * 25 +1));
+    matriz9.push(Math.floor(Math.random() * 25 +1));
+}
+
+matriz1B[0]=matriz5;
+matriz1B[1]=matriz6;
+matriz1B[2]=matriz7;
+matriz1B[3]=matriz8;
+matriz1B[4]=matriz9;
+
 console.table(matriz1A);
+console.table(matriz1B);
