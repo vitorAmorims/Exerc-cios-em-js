0180732a50d19151f025aa7602b1ba8ea7df9e4f
Vitor Amorims <vtamorims@gmail.com>
2020-08-22 11:07:59 -0300

    Criando as matriz para resoluçao do exercicio

---
+++ Ex_array_bidimensional1.js
@@ -0,0 +1,6 @@
+/*
+Elaborar um programa que leia duas matrizes A e B, cada uma de duas dimensões com cinco linhas e três colunas para valores inteiros. Construir uma matriz C de mesma dimensão, que seja formada pela soma dos elementos da Matriz A com elementos da matriz B. Apresentar os elementos da matriz C.
+*/
+var matriz1A = new Array;
+var matriz1B = new Array;
+var matriz1C = new Array;
