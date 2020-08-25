b1c20ac4294412d325c698212e019a24c16fe965
Vitor Amorims <vtamorims@gmail.com>
2020-08-25 07:32:15 -0300

    Media para primeira linha

--- exArrayBidimensional/Ex3_MediaDosAlunos.js
+++ exArrayBidimensional/Ex3_MediaDosAlunos.js
@@ -16,9 +16,12 @@ console.table(criarMatriz2D(5,5));
 
 //console.log(matriz[0][0]+matriz[0][1]); lógica para somar as linhas, antes de calcular média por linha.
 var soma = 0;
+var media = 0
 for (let i = 0; i < 1; i++){
     for (let j = 0; j < 5; j++){
         soma += (matriz[i][j]);
+        media = soma / matriz.length;
     }
 }
 console.log(soma); //cosegui criar soma para os itens da primeira linha.
+console.log(media); //consegui fazer a media para primeira linha.
